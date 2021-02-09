02f50a219680ed13f7e55c8ee81474ffe530a2c3
franek86 <frane.konjevoda@gmail.com>
2021-02-09 19:48:53 +0100

    style: create fancy underline, latest blogs: add date and read more

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
         <section class="home__hero h--vh">
             <div class="container">
                 <div class="grid columns-2">
-                    <h1 class="title bold">Welcome lorem ipsum</h1>
+                    <h1 class="title bold">Welcome <span class="underline">lorem</span> ipsum</h1>
                     <div></div>
                 </div>
                 
@@ -17,15 +17,24 @@
                 <div class="grid columns-1 columns-l-3">
                     <article class="latest__blogs_cart" v-for="latestPost in getLatestPosts" :key="latestPost.id">
                         <nuxt-link :to="{name:'blogs-id', params:{id: latestPost.id}}" class="blog__link">
-                            <div class="blog__overlay">
-                                <div class="latest__blogs_content">
-                                    <h1>{{latestPost.Title}}</h1>
-                                </div>
-                            </div>
-                            
+
+
                             <div class="latest__blogs_img">
+                                <div class="latest__blogs_date">{{latestPost.Date | moment("DD.MMM")}}</div>
                                 <img :src="`http://localhost:1337${latestPost.Image.url}`" alt="" />
                             </div>
+                            
+                            <div class="latest__blogs_content">
+                                <h1>{{latestPost.Title}}</h1>
+                            </div>
+
+                            <div class="readmore">
+                                <nuxt-link :to="{name:'blogs-id', params:{id: latestPost.id}}" class="blog__link"> 
+                                    Read More
+                                </nuxt-link>
+                            </div>
+                            
+                            
                         </nuxt-link>
                     </article>
                 </div>
@@ -67,6 +76,23 @@ export default {
 </script>
 
 <style lang="scss">
+
+.latest__blogs_img{
+    position: relative;
+    display: block;
+}
+
+.latest__blogs_date{
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    background-color: rgba($color: #000000, $alpha: 0.4);
+    font-size: 2.2rem;
+    padding: 10px;
+    color:$color-grey;
+}
+
+
     /*BIG TABELS VIEW min width 1024 px*/
 @media (min-width: $breakpoint-desktop){
     .latest__blogs_cart:first-child{
