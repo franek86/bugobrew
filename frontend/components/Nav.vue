3b7967f273e71e729a2322ba9c339a8def9ee2b9
franek86 <frane.konjevoda@gmail.com>
2021-03-02 22:48:49 +0100

    added backdrop to menu slide

--- frontend/components/Nav.vue
+++ frontend/components/Nav.vue
@@ -1,75 +1,95 @@
 <template>
-    <div>
-        <div class="slide__menu" 
-            @click="closeSlideMenu()" 
-            :class="{'slide__menu_active' : isSlideMenuOpen}">
-
-            <transition name="slide__menu">
-                <nav if="isSlideMenuOpen" class="slide__menu_nav">
-                    <slot></slot>
-                </nav>
-            </transition>
-        </div>
-
-        
+  <div>
+    <div
+      class="backdrop"
+      :class="{ backdrop_active: isSlideMenuOpen }"
+      @click="closeSlideMenu()"
+    ></div>
+    <div
+      class="slide__menu"
+      @click="closeSlideMenu()"
+      :class="{ slide__menu_active: isSlideMenuOpen }"
+    >
+      <transition name="slide__menu">
+        <nav if="isSlideMenuOpen" class="slide__menu_nav">
+          <slot></slot>
+        </nav>
+      </transition>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    /*data(){
+  /*data(){
         return{
             isSlideMenuOpen: false
         }
     },*/
 
-    computed:{
-        isSlideMenuOpen(){
-            return this.$store.state.isNavOpen
-        }
-    },
-
-    methods:{
-        closeSlideMenu(){
-            //this.isSlideMenuOpen = !this.isSlideMenuOpen
-            this.$store.dispatch('getNavOpen');
-        }
+  computed: {
+    isSlideMenuOpen() {
+      return this.$store.state.isNavOpen;
     }
-}
+  },
+
+  methods: {
+    closeSlideMenu() {
+      //this.isSlideMenuOpen = !this.isSlideMenuOpen
+      this.$store.dispatch("getNavOpen");
+    }
+  }
+};
 </script>
 
 <style lang="scss">
-    .slide__menu-enter-active,
-    .slide__menu-leave-active
-    {
-        transition: transform 0.2s ease;
-    }
+.slide__menu-enter-active,
+.slide__menu-leave-active {
+  transition: transform 0.2s ease;
+}
 
-    .slide__menu-enter,
-    .slide__menu-leave-to {
-        transform: translateX(-100%);
-        transition: all 150ms ease-in-out 0s
-    }
+.slide__menu-enter,
+.slide__menu-leave-to {
+  transform: translateX(-100%);
+  transition: all 150ms ease-in-out 0s;
+}
 
-    .slide__menu{
-        position: fixed;
-        top: 0;
-        right: -55vw;
-        background: $color-secondary;
-        min-height: 100vh;
-        width: 55vw;
-        transition: right 1s ease-in-out;
-        clip-path: polygon(11% 0, 100% 0%, 100% 100%, 0 100%);
-        height: 100%;
-        
+.backdrop {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba($color: #000000, $alpha: 0.4);
+  transform: translateX(100%);
+  transition: transform 0.2s ease-in-out 0s;
 
-        &.slide__menu_active{
-            right: 0;
-        }
+  &_active {
+    transform: translateX(0%);
+    transition: transform 0.15s ease-in-out 0s;
+  }
+}
 
-        .slide__menu_nav{
-            clip-path: polygon(11% 0, 100% 0%, 100% 100%, 0 100%);
-            height: 100%;
-        }
-    }
+.slide__menu {
+  position: fixed;
+  top: 0;
+  right: -55vw;
+  background: $color-secondary;
+  min-height: 100vh;
+  width: 55vw;
+  transition: right 1s ease-in-out;
+  clip-path: polygon(11% 0, 100% 0%, 100% 100%, 0 100%);
+  height: 100%;
+
+  &.slide__menu_active {
+    right: 0;
+  }
+
+  .slide__menu_nav {
+    clip-path: polygon(11% 0, 100% 0%, 100% 100%, 0 100%);
+    height: 100%;
+  }
+}
 </style>
--- frontend/components/UI/BaseSidebar.vue
+++ frontend/components/UI/BaseSidebar.vue
@@ -20,7 +20,7 @@ export default {
     title: {
       type: String,
       required: false,
-      defualt: "Sidebar"
+      default: "Sidebar"
     }
   }
 };
